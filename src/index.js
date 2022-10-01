313a6a35cfc38d8cff42cfc49b1c1025f6b05e9c
Smith <william.smith@bankerslife.com>
2022-10-01 12:13:54 -0400

    added THin to our existing project

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initThinBackend, ensureIsUser } from 'thin-backend';
+import { ThinBackend } from 'thin-backend-react';
+
+// This needs to be run before any calls to `query`, `createRecord`, etc.
+initThinBackend({
+    // This url is different for each backend, you can find the backend url in the project documentation
+    host: 'https://<YOUR PROJECTS BACKEND_URL>'
+});
+
+// React entrypoint component
+//function App() {
+    // The `requireLogin` triggers a redirect to the login page if not logged in already
+//    return <ThinBackend requireLogin>
+//        <div>Hello World</div>
+//    </ThinBackend>
+//}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
